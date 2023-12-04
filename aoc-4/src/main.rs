04a6845ea76b5e920a1de028185e970fc6d735ba
Marek Jansky <marek.jansky@ima.cz>
2023-12-04 10:40:49 +0100

    AoC4 part2 done

--- aoc-4/src/main.rs
+++ aoc-4/src/main.rs
@@ -1,9 +1,9 @@
-use std::collections::BTreeSet;
+use std::collections::{BTreeSet, VecDeque};
 
 fn main() {
     let input = include_str!("./input.txt");
-    let output = process1(input);
-    dbg!(output);
+    let output = process2(input);
+    println!("Result of computation: {output}");
 }
 
 #[allow(dead_code)]
@@ -41,13 +41,90 @@ fn process1(input : &str) -> String {
     game_sum.to_string()
 }
 
+#[derive(Default)]
+#[derive(Debug)]
+#[derive(Clone)]
+struct Card {
+    id : u32,
+    winning : BTreeSet<u32>,
+    drawn : BTreeSet<u32>
+}
+
+impl Card {
+    fn intersects(&mut self) -> Vec<u32>{
+        self.drawn.intersection(&self.winning).cloned().collect()
+    }
+
+    fn from_line(line: &str) -> Card {
+        let card_id = line.split(":")
+            .next()
+            .expect("Here is no last element")
+            .split(" ").last().expect("No card ID number")
+            .parse::<u32>()
+            .expect("It is not a correct type");
+        let card_game = line.split(":")
+            .last()
+            .expect("Here is no last element")
+            .split("|")
+            .collect::<Vec<_>>();
+
+        Card {
+            id: card_id,
+            winning: card_game.first().unwrap().trim().split(" ").map(|x |{
+                match x.parse::<u32>() {
+                    Ok(x) => x,
+                    Err(_) => 0
+                }
+            }).filter(|x|{
+                *x != 0
+            }).collect(),  
+            drawn: card_game.last().unwrap().trim().split(" ").map(|x |{
+                match x.parse::<u32>() {
+                    Ok(x) => x,
+                    Err(_) => 0
+                }
+            }).filter(|x|{
+                *x != 0
+            }).collect()
+        }
+    }
+}
 
 #[allow(dead_code)]
 fn process2(input : &str) -> String {
     
-    let game_sum = 0;
-    
-    game_sum.to_string()
+    let games_orig: Vec<Card> = input.lines().map(|line|{
+        Card::from_line(line)
+    }).collect();
+
+    let mut games = VecDeque::from(games_orig.clone());
+    let mut card_counts: Vec<i64> = vec![0;games.len()];
+
+    // dbg!(&games);
+    // for (idx, mut game) in games {
+    while games.len() > 0 {
+
+        let mut card = games.pop_front().expect("There should be at least one element");
+        
+        card_counts[(card.id-1) as usize] += 1;
+            
+        let intersections = card.intersects();
+        let no_cards = intersections.len() as u64;
+        // println!("new cards: {}, card_id = {}", no_cards, card.id);
+        // println!("intersect: {intersections:?}");
+
+        for new_offsets in 0..no_cards {
+            // dbg!(&new_id);
+            let new_id = new_offsets + card.id as u64 + 1;
+            let new_game = games_orig[(new_id-1) as usize].clone();
+            // dbg!(&new_game);
+            games.push_back(new_game);
+        }
+        // sleep(Duration::from_millis(500));
+    }
+
+    card_counts.iter().sum::<i64>().to_string()
+
 }
 
 #[cfg(test)]
@@ -57,16 +134,15 @@ mod tests {
     #[test]
     fn test_process1() {
         let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+    Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
         let exp_output = "13";
         assert_eq!(exp_output, process1(input));
     }
 
-    #[ignore]
     #[test]
     fn test_process2() {
         let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
@@ -75,7 +151,7 @@ Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
-        let exp_output = "2286";
+        let exp_output = "30";
         assert_eq!(exp_output, process2(input));
     }
 
